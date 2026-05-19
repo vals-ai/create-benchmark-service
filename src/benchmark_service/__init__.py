5e0c2417ac390e3406b4a5c74840693eceb9ba33
Jarett Forzano <jarett@vals.ai>
2026-05-18 22:06:07 -0400

    Type the benchmark service unauthenticated error

--- src/benchmark_service/__init__.py
+++ src/benchmark_service/__init__.py
@@ -3,12 +3,13 @@
 from benchmark_service._version import __version__
 from benchmark_service.app import BenchmarkServiceApp
 from benchmark_service.base import BenchmarkService
-from benchmark_service.client import BenchmarkServiceClient, BenchmarkServiceError
+from benchmark_service.client import BenchmarkServiceUnauthenticatedError, BenchmarkServiceClient, BenchmarkServiceError
 from benchmark_service.inflight import InflightMiddleware
 
 __all__ = [
     "BenchmarkServiceApp",
     "BenchmarkService",
+    "BenchmarkServiceUnauthenticatedError",
     "BenchmarkServiceClient",
     "BenchmarkServiceError",
     "InflightMiddleware",
